03b909eaf935193c21511a47b4af281b236f6b3c
prakhar <prakharbksc9@gmail>
2025-08-15 17:54:24 +0530

    navbar components

--- geekcode/src/pages/_app.tsx
+++ geekcode/src/pages/_app.tsx
@@ -2,15 +2,14 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return 
-  <>
-  <head>
-    <title>GeekCode</title>
-    <meta name="description" content="A platform for geeks to share and discover code." />
-    <link rel="icon" href="/favicon.ico" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-  </head>
+  return <>
 
-  <Component {...pageProps} />
+    <head>
+      <title>GeekCode</title>
+      <meta name = "viewport" content = "width=device-width, initial-scale=1" />
+      <meta name = "description" content = "GeekCode - Your coding companion" />
+      <link rel = "icon" href = "/favicon.ico" />
+    </head>
+    <Component {...pageProps} />
   </>
 }
