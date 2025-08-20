0b69a7d08ec3297fcefcabe8b3e496b8ab9efdc4
prakhar <prakharbksc9@gmail.com>
2025-08-20 11:54:02 +0530

    logo and sign in button

--- geekcode/src/Components/Navbar/Navbar.tsx
+++ geekcode/src/Components/Navbar/Navbar.tsx
@@ -5,13 +5,16 @@ type NavbarProps = {
 };
 
 const Navbar:React.FC<NavbarProps> = () => {
-    return <div className='flex items-center justify-between sm:px-12 px-2 md:px-24 '>
-        <Link href="/" className='flex items-center justify-center h-20'>
-            <img src="/logo.png" alt="GeekCode" className='h-full' />
+    return <div className='flex items-center justify-between sm:px-12 px-2 md:px-24 h-20'>
+        <Link href="/" className='flex items-center h-full'>
+            <img src="/logo-full.png" alt="GeekCode" className='h-full' />
         </Link>
         <div className="flex items-center">
-            <button className='bg-brand-orange text-white px-2 py-1 sm:px-4 rounded-md text-sm font-medium
-            hover:text-brand-orange hover:bg-white hover:border-2 hover:border-brand-orange '>
+            {/* Sign In button with brand-orange background and black hover effect */}
+            <button 
+                style={{backgroundColor: 'rgb(21, 194, 70)'}} 
+                className='text-white px-3 py-2 sm:px-4 rounded-md text-sm font-medium
+                hover:text-white hover:bg-black hover:border-2 hover:border-brand-orange'>
                 Sign In
             </button>
         </div>
