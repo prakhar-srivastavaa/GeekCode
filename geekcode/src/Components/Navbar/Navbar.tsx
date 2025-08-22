926fa6f2b8ec57f4556f4704ff178fe3261aa642
prakhar <prakharbksc9@gmail.com>
2025-08-22 16:17:52 +0530

    Navbar final

--- geekcode/src/Components/Navbar/Navbar.tsx
+++ geekcode/src/Components/Navbar/Navbar.tsx
@@ -6,32 +6,18 @@ type NavbarProps = {
 
 const Navbar:React.FC<NavbarProps> = () => {
     return <>
-    <div className="navbar bg-base-100 shadow-">
+    <div className="navbar shadow- display-none m-auto h-[50px] w-full items-center justify-center px-6 md:flex bg-base-200">
+
+
+    {/* <div className="navbar bg-base-200 shadow-"> */}
   <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-      </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
-    </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    {/* add Z:\Backend Java\GeekCode\geekcode\public\logo-full.png this logo and make is small as per navbar  * also add "/home" when user clicks it takes to homw page*/}
+    <Link href="/home"><img src="/logo-full.png" alt="Logo" className="h-16 w-32"/></Link>
   </div>
   <div className="navbar-center hidden lg:flex">
   </div>
   <div className="navbar-end">
-    <a className="btn">Button</a>
+    <a className="btn btn-sm btn-ghost bg-green-800 hover:bg-green-700 text-white">Sign in</a>
   </div>
 </div>
     </>
