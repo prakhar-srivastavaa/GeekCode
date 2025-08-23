b3e3a25a5fe409ed989a476440bb344d28da873d
prakhar <prakharbksc9@gmail.com>
2025-08-23 11:02:22 +0530

    Navbar -done

--- geekcode/src/pages/auth/index.tsx
+++ geekcode/src/pages/auth/index.tsx
@@ -1,4 +1,6 @@
 import Navbar from '@/Components/Navbar/Navbar';
+import Coding from '@/Components/Coding/Coding';
+
 import React from 'react';
 
 type AuthPageProps = {
@@ -10,6 +12,9 @@ const AuthPage:React.FC<AuthPageProps> = () => {
     return <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
         <div className="mx-auto">
             <Navbar />
+            <div className='flex justify-center'>
+
+            </div>
         </div>
     </div>;
 };
