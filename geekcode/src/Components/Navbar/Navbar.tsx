1c39587da6720a5fe49cac239df82624a4cb4ac4
prakhar <prakharbksc9@gmail.com>
2025-08-25 14:27:59 +0530

    definde all colour in globalcss

--- geekcode/src/Components/Navbar/Navbar.tsx
+++ geekcode/src/Components/Navbar/Navbar.tsx
@@ -1,28 +1,28 @@
 import Link from 'next/link';
 import React from 'react';
 type NavbarProps = {
-    
+
 };
 
-const Navbar:React.FC<NavbarProps> = () => {
-    return (
+const Navbar: React.FC<NavbarProps> = () => {
+  return (
     <>
-    <div className="navbar shadow- display-none m-auto h-[50px] w-full items-center justify-center px-6 md:flex bg-base-200">
-
-
-    {/* <div className="navbar bg-base-200 shadow-"> */}
-  <div className="navbar-start">
-    {/* add Z:\Backend Java\GeekCode\geekcode\public\logo-full.png this logo and make is small as per navbar  * also add "/home" when user clicks it takes to homw page*/}
-    <Link href="/home"><img src="/logo-full.png" alt="Logo" className="h-16 w-32"/></Link>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-  </div>
-  <div className="navbar-end">
-    <a className="btn btn-sm btn-ghost bg-green-800 hover:bg-green-700 text-white">Sign in</a>
-  </div>
-</div>
+      <div className="navbar shadow- display-none m-auto h-[50px] w-full items-center justify-center px-6 md:flex bg-base-200">
+        <div className="navbar-start">
+          <Link href="/home"><img src="/logo-full.png" alt="Logo" className="h-16 w-32" /></Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+        </div>
+        {/* add herif link/auth-login when clicked button signin*/}
+        <div className="navbar-end">
+          <Link href="/auth-login">
+          <button className="btn btn-sm btn-ghost bg-green-800 hover:bg-green-700 text-white">Sign in</button>
+</Link>
+          {/* <a className="btn btn-sm btn-ghost bg-green-800 hover:bg-green-700 text-white">Sign in</a> */}
+        </div>
+      </div>
     </>
-    );
+  );
 }
 
 export default Navbar;
