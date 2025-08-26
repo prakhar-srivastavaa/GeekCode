7f10e8efda013027794d49df7dbccaf1d0cdf8b3
prakhar <prakharbksc9@gmail.com>
2025-08-26 10:54:47 +0530

    fix

---
+++ geekcode/src/Components/Modals/ResetPassword.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type ResetPasswordProps = {
+    
+};
+
+const ResetPassword:React.FC<ResetPasswordProps> = () => {
+    
+    return <div>Reset  Password</div>
+}
+export default ResetPassword;
