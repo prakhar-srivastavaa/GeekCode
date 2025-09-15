1d6687c804bdaadc1db187ae16b190af7b63aabf
prakhar <prakharbksc9@gmail.com>
2025-09-15 15:49:58 +0530

    fullscreen btn and setting button ui done

--- geekcode/src/Components/Workspace/Playground/PreferenceNav/PreferenceNav.tsx
+++ geekcode/src/Components/Workspace/Playground/PreferenceNav/PreferenceNav.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AiOutlineFullscreen, AiOutlineSetting } from 'react-icons/ai';
 
 type PreferenceNavProps = {
 
@@ -15,7 +16,25 @@ const PreferenceNav: React.FC<PreferenceNavProps> = () => {
                     </div>
                 </button>
             </div>
-            <div>
+            <div className='flex items-center m-2'>
+                <button className='relative rounded px-3 py-1.5 font-medium items-center transition-all focus:outline-none inline-flex ml-auto p-1 mr-2 hover:bg-dark-fill-3 group'>
+                    <div className='h-4 w-4 text-dark-gray-6 font-bold text-lg'>
+                        <AiOutlineSetting />
+                    </div>
+                    <div className='absolute w-auto p-2 text-sm m-2 min-w-max translate-x-3 right-0 top-5 z-10 rounded-md shadow-md text-dark-layer-2 bg-gray-200 origin-center scale-0 transition-all duration-100 ease-linear group-hover:scale-100'>
+                        Settings
+                    </div>
+                </button>
+
+
+                <button className='relative rounded px-3 py-1.5 font-medium items-center transition-all focus:outline-none inline-flex ml-auto p-1 mr-2 hover:bg-dark-fill-3 group'>
+                    <div className='h-4 w-4 text-dark-gray-6 font-bold text-lg'>
+                        <AiOutlineFullscreen />
+                    </div>
+                    <div className='absolute w-auto p-2 text-sm m-2 min-w-max translate-x-3 right-0 top-5 z-10 rounded-md shadow-md text-dark-layer-2 bg-gray-200 origin-center scale-0 transition-all duration-100 ease-linear group-hover:scale-100'>
+                        Full Screen
+                    </div>
+                </button>
 
             </div>
         </div>
