2cef843f85e0f12e0c7467dd5bcdaac6dff3a6e9
prakhar <prakharbksc9@gmail.com>
2025-09-16 00:57:01 +0530

    workspace twosum done

--- geekcode/src/Components/Workspace/Playground/Playground.tsx
+++ geekcode/src/Components/Workspace/Playground/Playground.tsx
@@ -4,40 +4,80 @@ import Split from 'react-split';
 import CodeMirror from '@uiw/react-codemirror';
 import { vscodeDark } from '@uiw/codemirror-theme-vscode';
 import { javascript } from '@codemirror/lang-javascript';
+import EditorFooter from './EditorFooter';
 
 type PlaygroundProps = {
-    
+
 };
 
-const Playground:React.FC<PlaygroundProps> = () => {
-    
-    return (
-    <div className='flex flex-col bg-dark-layer-1 relative'>
-        <PreferenceNav/>
+const Playground: React.FC<PlaygroundProps> = () => {
 
-        <Split className='h-[calc(100vh-94px)]' direction='vertical' sizes={[60,40]} minSize={60} >
+    return (
+        <div className='flex flex-col bg-dark-layer-1 relative'>
+            <PreferenceNav />
+
+            <Split className='h-[calc(100vh-94px)]' direction='vertical' sizes={[60, 40]} minSize={60} >
                 <div className="w-full overflow-auto">
                     <CodeMirror
                         value="const a=1;"
                         theme={vscodeDark}
                         extensions={[javascript()]}
-                        style={{fontSize: 16}}
-                     />
+                        style={{ fontSize: 16 }}
+                    />
                 </div>
                 <div className='w-full px-5 overflow-auto'>
                     {/* testcases heading */}
                     <div className='flex h-10 items-center space-x-6'>
                         <div className='relative flex h-full flex-col justify-center cursor-pointer'>
                             <div className='text-sm font-medium leading-5 text-white'>Testcase</div>
-                            <hr className='absolute bottom-0 h-0.5 w-full rounded-full border-none bg-white'/>
+                            <hr className='absolute bottom-0 h-0.5 w-full rounded-full border-none bg-white' />
                         </div>
                     </div>
-                    {/* testcasees */}
-                    <button data-e2e-locator="console-testcase-tag" class="font-medium items-center whitespace-nowrap focus:outline-none inline-flex bg-fill-3 dark:bg-dark-fill-3 hover:bg-fill-2 dark:hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 hover:text-label-1 dark:hover:text-dark-label-1 text-label-1 dark:text-dark-label-1" jf-ext-button-ct="case 1">Case 1</button>
 
+                    <div className='flex'>
+                        {/* case1 */}
+                        <div className="mr-2 items-start mt-2">
+                            <div className='flex flex-wrap items-center gap-y-4'>
+                                <div
+                                    className={`font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap`}>
+                                    Case 1
+                                </div>
+                            </div>
+                        </div>
+                        {/* case2 */}
+                        <div className="mr-2 items-start mt-2">
+                            <div className='flex flex-wrap items-center gap-y-4'>
+                                <div
+                                    className={`font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap`}>
+                                    Case 2
+                                </div>
+                            </div>
+                        </div>
+                        {/* case3 */}
+                        <div className="mr-2 items-start mt-2">
+                            <div className='flex flex-wrap items-center gap-y-4'>
+                                <div
+                                    className={`font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap`}>
+                                    Case 3
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='font-semibold my-4'>
+                        <p className='text-sm font-medium mt-4 text-white'>Input:</p>
+                        <div className='w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2'>
+                            nums: [2,7,11,15], target = 9
+                        </div>
+                        <p className='text-sm font-medium mt-4 text-white'>Output:</p>
+                        <div className='w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2'>
+                            [0,1]
+                        </div>
+                    </div>
                 </div>
-        </Split>
-    </div>
+            </Split>
+            <EditorFooter />
+        </div>
     )
 }
 export default Playground;
