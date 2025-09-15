0742d4d5cc53001bb6d45c193a2a478dc8d0d220
prakhar <prakharbksc9@gmail.com>
2025-09-16 00:25:33 +0530

    improve ui for split virtical and horizontal

--- geekcode/src/Components/Workspace/Playground/Playground.tsx
+++ geekcode/src/Components/Workspace/Playground/Playground.tsx
@@ -24,7 +24,18 @@ const Playground:React.FC<PlaygroundProps> = () => {
                         style={{fontSize: 16}}
                      />
                 </div>
-                <div>testcase</div>
+                <div className='w-full px-5 overflow-auto'>
+                    {/* testcases heading */}
+                    <div className='flex h-10 items-center space-x-6'>
+                        <div className='relative flex h-full flex-col justify-center cursor-pointer'>
+                            <div className='text-sm font-medium leading-5 text-white'>Testcase</div>
+                            <hr className='absolute bottom-0 h-0.5 w-full rounded-full border-none bg-white'/>
+                        </div>
+                    </div>
+                    {/* testcasees */}
+                    <button data-e2e-locator="console-testcase-tag" class="font-medium items-center whitespace-nowrap focus:outline-none inline-flex bg-fill-3 dark:bg-dark-fill-3 hover:bg-fill-2 dark:hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 hover:text-label-1 dark:hover:text-dark-label-1 text-label-1 dark:text-dark-label-1" jf-ext-button-ct="case 1">Case 1</button>
+
+                </div>
         </Split>
     </div>
     )
