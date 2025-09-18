98799c72957d5f7ddb64fe6a8f2ab683f5e01704
prakhar <prakharbksc9@gmail.com>
2025-09-18 23:01:02 +0530

    landend on twosum

--- geekcode/src/pages/problems/[pid].tsx
+++ geekcode/src/pages/problems/[pid].tsx
@@ -1,8 +1,11 @@
 import Topbar from '@/Components/Topbar/Topbar';
 import Workspace from '@/Components/Workspace/Workspace';
+import { problems } from '@/utils/problems';
+import { GetStaticProps } from 'next';
 import React from 'react';
 
 type ProblemPageProps = {
+    problem: any;
     
 };
 
@@ -17,3 +20,34 @@ export default ProblemPage;
 
 //fetch the loacal data
 //ssg:static site generation
+//get staticpaths => it creates the dynamic routes
+export async function getStaticPaths() {
+    const paths = Object.keys(problems).map((key) => ({
+        params: { pid: key },
+    }));
+    return { paths, fallback: false };
+}
+
+//get static props => it fetches the data for each route
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    const { pid } = params as { pid: string };
+
+    // Get the problem data
+    const problem = problems[pid];
+
+    // Remove the handlerFunction before returning
+    if (problem) {
+        const { handlerFunction, ...problemWithoutHandler } = problem;
+
+        return {
+            props: {
+                problem: problemWithoutHandler,
+            },
+        };
+    }
+
+    return {
+        notFound: true,
+    };
+};
+
