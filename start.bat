2823151ee0ee495ce3abd4e0e35155710885f024
prakhar <prakharbksc9@gmail.com>
2025-10-08 15:24:40 +0530

    start.bat added

---
+++ start.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Starting Full Stack Application...
+@REM cd /d "C:\Users\amank\OneDrive\Desktop\projects\swe-agent-project\backend"
+@REM start node server.js
+cd /d "Z:\Backend Java\GeekCode"
+npm run dev
