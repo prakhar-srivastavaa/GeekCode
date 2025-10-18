6ed92ddebde369c15fa660caee6f2afa6bd11da6
Prakhar Srivastava <73735817+prakhar-srivastavaa@users.noreply.github.com>
2025-10-18 19:45:02 +0530

    Change directory to GeekCode for npm dev start

--- start.bat
+++ start.bat
@@ -3,4 +3,5 @@ echo Starting Full Stack Application...
 @REM cd /d "C:\Users\amank\OneDrive\Desktop\projects\swe-agent-project\backend"
 @REM start node server.js
 cd /d "Z:\Backend Java\GeekCode"
+
 npm run dev
