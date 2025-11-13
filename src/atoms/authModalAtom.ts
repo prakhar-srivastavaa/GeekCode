5542cdb4f302a46ee6f4a09f08ba9e5ddabbc2aa
prakhar-srivastavaa <prakharbksc9@gmail.com>
2025-11-13 16:14:11 +0530

    auth-v2

--- src/atoms/authModalAtom.ts
+++ src/atoms/authModalAtom.ts
@@ -1,14 +1,16 @@
 import {atom} from "recoil";
+
 type AuthModalState = {
     isOpen: boolean;
     type: "login" | "register" | "forgotPassword";
 };
+
 const initalAuthModalState: AuthModalState = {
     isOpen: false,
     type: "login",
 };
 
 export const authModalState = atom<AuthModalState>({
-    key: "authModalState",
+    key: "authModalState_v1",
     default: initalAuthModalState,
 });
